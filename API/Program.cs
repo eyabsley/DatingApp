50a00e7a0134271bce2e7efe49a6ada2c10814ed
eyabsley <ed@yabsley.com>
2025-03-05 17:26:58 +0000

    End of section 3

--- API/Program.cs
+++ API/Program.cs
@@ -17,7 +17,7 @@ var app = builder.Build();
 
 //Configure the HTTP request pipeline
 app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod()
-.WithOrigins("http://localhost:4200","http://localhost:4200" ));
+.WithOrigins("http://localhost:4200","https://localhost:4200" ));
 app.MapControllers();
 
 app.Run();
